9bb70da34ef6b9c128669af31edfe65b2a244ea2
Brayder Berroteran <brayderb@gmail.com>
2019-03-04 16:29:41 -0300

    add user companies and claims to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Company.destroy_all
+Claim.destroy_all
+
+30.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: 123456
+  )
+end
+users = User.all
+
+20.times do
+  Company.create(
+    name: Faker::Company.name
+  )
+end
+companies = Company.all
+
+250.times do
+  Claim.create(
+    content: Faker::Company.type,
+    company: companies.sample
+  )
+end
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if AdminUser.where(email: 'admin@example.com').blank?
 
 
